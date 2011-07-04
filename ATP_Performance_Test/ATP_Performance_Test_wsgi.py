4ae66fa552858d566fa6fc99ef2928a08529f49c
Tomek Kopczuk <tomek@bladepolska.com>
2011-07-04 18:47:50 +0200

    Gondor seem to require one level and two levels up from settings.py on PYTHONPATH

--- ATP_Performance_Test/ATP_Performance_Test_wsgi.py
+++ ATP_Performance_Test/ATP_Performance_Test_wsgi.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ATP_Performance_Test.settings'
 
 import django.core.handlers.wsgi
