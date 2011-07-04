4e9e1b52aa1b4164349692714d846a183893f225
Tomek Kopczuk <tomek@bladepolska.com>
2011-07-04 19:46:56 +0200

    Gondor paths.

--- ATP_Performance_Test_wsgi.py
+++ ATP_Performance_Test_wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ATP_Performance_Test.settings'
 
 import django.core.handlers.wsgi
