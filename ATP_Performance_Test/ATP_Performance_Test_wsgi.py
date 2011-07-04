43b3379f331d0a5c330c61e9350dec4b752c018c
Tomek Kopczuk <tomek@bladepolska.com>
2011-07-04 19:51:46 +0200

    Unified layout, once again.

--- ATP_Performance_Test/ATP_Performance_Test_wsgi.py
+++ ATP_Performance_Test/ATP_Performance_Test_wsgi.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ATP_Performance_Test.settings'
 
 import django.core.handlers.wsgi
