1283ba9d68e4811f6b4c3f517aaee7303fe90c43
Tomek Kopczuk <tomek@bladepolska.com>
2011-07-05 22:28:09 +0200

    Yet another try for DotCloud.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = '.'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ATP_Performance_Test.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
